80ad7b3036980bccc32cc7f6bd66dfcc094e59d4
stevie1mat <mathewsteven92@gmail.com>
2019-08-14 06:21:48 +0530

    Jquery Upload

---
+++ script.js
@@ -0,0 +1,31 @@
+$("#inputGroupFile01").change(function(event) {  
+  RecurFadeIn();
+  readURL(this);    
+});
+$("#inputGroupFile01").on('click',function(event){
+  RecurFadeIn();
+});
+function readURL(input) {    
+  if (input.files && input.files[0]) {   
+    var reader = new FileReader();
+    var filename = $("#inputGroupFile01").val();
+    filename = filename.substring(filename.lastIndexOf('\\')+1);
+    reader.onload = function(e) {
+      debugger;      
+      $('#blah').attr('src', e.target.result);
+      $('#blah').hide();
+      $('#blah').fadeIn(500);      
+      $('.custom-file-label').text(filename);             
+    }
+    reader.readAsDataURL(input.files[0]);    
+  } 
+  $(".alert").removeClass("loading").hide();
+}
+function RecurFadeIn(){ 
+  console.log('ran');
+  FadeInAlert("Wait for it...");  
+}
+function FadeInAlert(text){
+  $(".alert").show();
+  $(".alert").text(text).addClass("loading");  
+}
